3be80110cba09ac82cb09b3d6a1cbf1b7c21d5b4
Josephine Jelkeby Ginguene <josephine.ginguene@gmail.com>
2026-03-11 20:23:39 +0100

    Adding jump feature

--- src/main/java/se/yrgo/Game.java
+++ src/main/java/se/yrgo/Game.java
@@ -4,14 +4,19 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Game extends ApplicationAdapter {
     private float characterY = 540;
     private float velocity = 600;
     private float gravity = -1500;
 
+    private ShapeRenderer shaperenderer; // Tilläfllig karaktär
+
+
     @Override
     public void create() {
+        shaperenderer = new ShapeRenderer();
         // För att initiera resurser, t.ex. texturer, bilder, fonts etc
     }
 
@@ -20,13 +25,38 @@ public class Game extends ApplicationAdapter {
         Gdx.gl.glClearColor(0.5f, 0.8f, 1f, 1);  // Tillfälligt för att se bakgrund
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            velocity = 600;
+        }
+
+        float screenHeight = Gdx.graphics.getHeight();
+        if (characterY > screenHeight) {
+            characterY = screenHeight;
+            velocity = 0;
+        }
+
+        if (characterY < 0) { // Tillfälligt för hindra karaktär att ramla ner utanför skärmen
+            characterY = 0;
+            if (velocity < 0) {
+                velocity = 0;
+            }
+        }
+
         float delta = Gdx.graphics.getDeltaTime();
         velocity += gravity * delta;
         characterY += velocity * delta;
+
+        shaperenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shaperenderer.setColor(1, 1, 0, 1);
+        shaperenderer.circle(910, characterY, 30);
+        shaperenderer.end();
+
+
     }
 
     @Override
     public void dispose() {
+        shaperenderer.dispose();
         // För att stänga resurser
     }
 }
