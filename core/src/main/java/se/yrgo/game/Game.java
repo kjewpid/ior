6076561f0e02ca5c17628d60c614923382d09fc8
Josephine Jelkeby Ginguene <josephine.ginguene@gmail.com>
2026-03-19 13:15:48 +0100

    adding obstacles

--- core/src/main/java/se/yrgo/game/Game.java
+++ core/src/main/java/se/yrgo/game/Game.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+
 public class Game extends ApplicationAdapter {
     private float characterY = 540;
     private float velocity = 600;
@@ -26,11 +28,22 @@ public class Game extends ApplicationAdapter {
     private float characterX = 400;
     private float flySpeed = 600;
 
+    //Hinder
+
+    private float obstacleDistance = 500;
+    private float obstacleSpeed = 200;
+    private float spawnRate = obstacleDistance / obstacleSpeed;
+    private Texture obstacleImage;
+    private ArrayList<Obstacle> obstacles = new ArrayList<>();
+    private float spawnTimer = 0;
+    private int totalObstaclesSpawned = 0;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
         characterImage = new Texture(Gdx.files.internal("Character.png"));
         startImage = new Texture(Gdx.files.internal("StartImage.png"));
+        obstacleImage = new Texture("Obstacle.JPG");
         buttonX = (Gdx.graphics.getWidth() - buttonWidth) / 2;
         buttonY = (Gdx.graphics.getHeight() - buttonHeight) / 2;
     }
@@ -85,6 +98,41 @@ public class Game extends ApplicationAdapter {
 
         velocity += gravity * delta;
         characterY += velocity * delta;
+
+        spawnTimer += delta;
+
+        if (spawnTimer > spawnRate) { // Spawna hinder varje x sekund
+            float gapHeight = 300;
+            float gapY = (float) (Math.random() * (Gdx.graphics.getHeight() - gapHeight - 50));
+            obstacles.add(new Obstacle(Gdx.graphics.getWidth(), gapY, obstacleImage));
+            spawnTimer -= spawnRate;
+
+            totalObstaclesSpawned++;
+
+            if (totalObstaclesSpawned % 10 == 0) { // Öka spawnrate och speed
+                obstacleSpeed *= 1.1f;
+                if (obstacleSpeed == 800) { // Max speed
+                    obstacleSpeed = 800;
+                }
+                spawnRate = obstacleDistance / obstacleSpeed;
+            }
+        }
+
+        batch.begin();
+        for (Obstacle o : obstacles) {
+            batch.draw(obstacleImage, o.getX(), 0, 100, o.getGapY());
+
+            batch.draw(obstacleImage, o.getX(), o.getGapY() + o.getGapHeight(),
+                100, Gdx.graphics.getHeight() - (o.getGapY() + o.getGapHeight()),
+                0, 0, obstacleImage.getWidth(), obstacleImage.getHeight(),
+                false, true);
+            
+        }
+        batch.end();
+
+        obstacles.forEach(o -> o.update(delta, obstacleSpeed));
+        obstacles.removeIf(o -> o.getX() + 100 < 0);
+
     }
 
     // NY METOD FÖR HOPP
@@ -97,5 +145,6 @@ public class Game extends ApplicationAdapter {
         batch.dispose();
         characterImage.dispose();
         startImage.dispose();
+        obstacleImage.dispose();
     }
 }
---
+++ core/src/main/java/se/yrgo/game/Obstacle.java
@@ -0,0 +1,38 @@
+package se.yrgo.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Obstacle {
+    private float x;
+    private float gapY;
+    private float gapHeight = 300;
+    //private Texture texture;
+
+    public Obstacle(float x, float gapY, Texture texture) {
+        this.x = x;
+        this.gapY = gapY;
+        // this.texture = texture;
+    }
+
+    public void update(float deltaTime, float speed) {
+        x -= speed * deltaTime;
+    }
+
+    public boolean isOffScreen() {
+        // return x + texture.getWidth() < 0;
+        return x + 50 < 0;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getGapY() {
+        return gapY;
+    }
+
+    public float getGapHeight() {
+        return gapHeight;
+    }
+}
+
