c0e092d442128eb3708cb48845964966ee9b8788
gustavlaveback1 <ac75138@skola.goteborg.se>
2026-03-19 13:24:26 +0100

    Added score, highscore, and highscore sound feature

--- core/src/main/java/se/yrgo/game/Game.java
+++ core/src/main/java/se/yrgo/game/Game.java
@@ -3,9 +3,13 @@ package se.yrgo.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.audio.Sound;
 
 public class Game extends ApplicationAdapter {
     private float characterY = 540;
@@ -13,37 +17,81 @@ public class Game extends ApplicationAdapter {
     private float gravity = -1500;
     private boolean gameStarted = false;
     private SpriteBatch batch;
-
-    //StartImage
+    
+    // StartImage
     private float buttonWidth = 400;
     private float buttonHeight = 200;
     private float buttonX, buttonY;
     private Texture startImage;
-
-    //Karaktär
+    
+    // Karaktär
     private Texture characterImage;
     private float startX = -120;
     private float characterX = 400;
     private float flySpeed = 600;
-
+    
+    // Poäng
+    private ScoreManager scoreManager;
+    private BitmapFont font;
+    private GlyphLayout layout;
+    private Sound highscoreSound;
+    private boolean newHighscorePlayed = false;
     @Override
+
     public void create() {
         batch = new SpriteBatch();
         characterImage = new Texture(Gdx.files.internal("Character.png"));
         startImage = new Texture(Gdx.files.internal("StartImage.png"));
         buttonX = (Gdx.graphics.getWidth() - buttonWidth) / 2;
         buttonY = (Gdx.graphics.getHeight() - buttonHeight) / 2;
+
+        scoreManager = new ScoreManager();
+        font = new BitmapFont();
+        font.setColor(1, 0, 0, 1);
+        font.getData().setScale(3);
+
+        layout = new GlyphLayout();
+
+        highscoreSound = Gdx.audio.newSound(Gdx.files.internal("HighScoreSound.wav"));
+    }
+
+    public void score() {
+        String scoreText = "Score: " + scoreManager.getScore();
+        String highScoreText = "Highscore: " + scoreManager.getHighScore();
+
+        layout.setText(font, scoreText);
+        float x = (Gdx.graphics.getWidth() - layout.width) / 2;
+        float y = Gdx.graphics.getHeight() - 100;
+        font.draw(batch, scoreText, x, y);
+
+        font.draw(batch, highScoreText, 20, Gdx.graphics.getHeight() - 20);
+
+        if (Gdx.input.justTouched()) {
+            scoreManager.incrementPoint();
+        }
+
+        /*
+         * Lägg till denna när vi har skaffat hinder, för att ge poäng för hinder
+         * istället
+         * if (obstacleX < characterX && !obstacleCounted) {
+         * scoreManager.incrementPoint();
+         * obstacleCounted = true;
+         * }
+         */
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0.5f, 0.8f, 1f, 1);  // Tillfälligt för att se bakgrund
+        Gdx.gl.glClearColor(0.5f, 0.8f, 1f, 1); // Tillfälligt för att se bakgrund
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         if (!gameStarted && Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             gameStarted = true;
             jump();
+            newHighscorePlayed = false;
+            scoreManager.resetScore();
         }
+
 
         if (!gameStarted) {
             batch.begin();
@@ -79,8 +127,17 @@ public class Game extends ApplicationAdapter {
             }
         }
 
+        if (scoreManager.getScore() == scoreManager.getHighScore()
+                && scoreManager.getScore() > 0
+                && !newHighscorePlayed) {
+
+            highscoreSound.play(0.7f);
+            newHighscorePlayed = true;
+        }
+
         batch.begin();
         batch.draw(characterImage, startX - 30, characterY - 30, 120, 120);
+        score();
         batch.end();
 
         velocity += gravity * delta;
@@ -97,5 +154,6 @@ public class Game extends ApplicationAdapter {
         batch.dispose();
         characterImage.dispose();
         startImage.dispose();
+        highscoreSound.dispose();
     }
 }
---
+++ core/src/main/java/se/yrgo/game/ScoreManager.java
@@ -0,0 +1,31 @@
+package se.yrgo.game;
+
+public class ScoreManager {
+    private int score;
+    private int highScore;
+
+    public ScoreManager() {
+        this.score = 0;
+        this.highScore = 0;
+    }
+
+    public void incrementPoint() {
+        score++;
+        if (score > highScore) {
+            highScore = score;
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public void resetScore() {
+        score = 0;
+    }
+
+}
