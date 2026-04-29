0a711600694a129e740389f52950f5b743408f89
gustavlaveback1 <ac75138@skola.goteborg.se>
2026-04-29 15:22:22 +0200

    Changed font on score and highscore

--- core/src/main/java/se/yrgo/game/renderers/ScoreRenderer.java
+++ core/src/main/java/se/yrgo/game/renderers/ScoreRenderer.java
@@ -2,6 +2,7 @@ package se.yrgo.game.renderers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -21,9 +22,10 @@ public class ScoreRenderer {
     }
 
     public void loadAssets() {
-        font = new BitmapFont();
-        font.setColor(1, 0, 0, 1);
-        font.getData().setScale(3);
+        font = new BitmapFont(Gdx.files.internal("font/fontWOD.fnt"));
+
+        font.getData().setScale(0.7f);
+        // font.setColor(Color.WHITE);
 
         layout = new GlyphLayout();
 
@@ -37,9 +39,9 @@ public class ScoreRenderer {
 
         layout.setText(font, scoreText);
         float x = (worldWidth - layout.width) / 2;
-        float y = worldHeight- 100;
-        font.draw(batch, scoreText, x, y);
+        font.draw(batch, scoreText, x, worldHeight - 100);
 
+        layout.setText(font, highScoreText);
         font.draw(batch, highScoreText, 20, worldHeight - 20);
 
         if (scoreManager.getScore() > highScoreAtStart && !newHighscorePlayed) {
